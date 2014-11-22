7b42d8ae6b0552c5bae623820680c6dbe73cb884
Heiko Rothe <mkerix@outlook.com>
2014-11-22 17:32:35 +0100

    Added snow objects

---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameController : MonoBehaviour {
+    public GameObject snowObject;
+    int score = 0;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("CreateSnow", 0.5f, 3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.color = Color.black;
+        GUILayout.Label("Score: " + score.ToString());
+    }
+
+    void CreateSnow()
+    {
+        Instantiate(snowObject);
+        score++;
+    }
+}
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class player : MonoBehaviour {
-	public Vector2 jumpForce = new Vector2(0, 400);
+	public Vector2 jumpForce = new Vector2(0, 700);
 	private Transform groundCheck;
 	public AudioClip jumpSound;
 
@@ -20,12 +20,22 @@ public class player : MonoBehaviour {
          if(Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
 			Jump();
 		}
+
+         Vector2 playerPos = Camera.main.WorldToScreenPoint(transform.position);
+         if (playerPos.x < -(Screen.width/2))
+         {
+             Die();
+         }
     } 
 
 	void Jump() {
 		rigidbody2D.AddForce (jumpForce);
 		audio.PlayOneShot(jumpSound);
 	}
+
+    void Die() {
+        Application.LoadLevel(Application.loadedLevel);
+    }
 
 	bool IsGrounded () {
 		return Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
