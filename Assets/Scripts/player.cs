db021d2dcb11f99fa380a26776e5aec53c88a6de
haiClass <haiClass@outlook.com>
2014-11-22 15:55:46 +0100

    Added player jumping

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -2,18 +2,32 @@
 using System.Collections;
 
 public class player : MonoBehaviour {
-    public float moveSpeed = 2f;
-    private float movement;
+	public Vector2 jumpForce = new Vector2(0, 300);
+	private Transform groundCheck;
+
+	void Awake() {
+		groundCheck = transform.Find ("groundCheck");
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        movement = Input.GetAxis("Horizontal") * moveSpeed;  
-        movement *= Time.deltaTime;        
-        transform.Translate(movement,0.0f, 0.0f); // move player along X axis  
+         if(Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
+			Jump();
+		}
     } 
+
+	void Jump() {
+		rigidbody2D.velocity = Vector2.zero;
+		rigidbody2D.AddForce (jumpForce);
+	}
+
+	bool IsGrounded () {
+		return Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+	}
+
 }
