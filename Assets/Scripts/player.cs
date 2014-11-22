b93da02854b4fe0674f82727f5fee3c5aff0454c
Heiko Rothe <mkerix@outlook.com>
2014-11-22 14:31:14 +0100

    Initial commit

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+    public float moveSpeed = 2f;
+    private float movement;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movement = Input.GetAxis("Horizontal") * moveSpeed;  
+        movement *= Time.deltaTime;        
+        transform.Translate(movement,0.0f, 0.0f); // move player along X axis  
+    } 
+}
